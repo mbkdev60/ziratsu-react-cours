d774fd13df7a67b0d63ab1c06030ac0638bafe80
moez <bkmoez2410@gmail.com>
2023-02-02 10:46:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+//import Item from './Components/Item/Item'
 
 function App() {
+  const [toggle, setToggle] = useState(true);
+
+  const changeState = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {toggle && <h1>Le state est True</h1>}
+      <button onClick={changeState}>Change state</button>
     </div>
   );
 }
